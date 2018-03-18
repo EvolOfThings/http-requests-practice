5f1cd44e3d2e8bbba93d5711a5dfccd03ed458cf
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-18 09:07:01 +0530

    Fetching data on update using componentDidUpdate method but this causes infinite loop so to avoid nested if conditions are required and keeping in mind fetching data is async

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate () {
+        if (this.props.id) {
+        //to make sure if we already have loaded the post to prevent inifinite looping
+        //and also checking if it was the same post so no need to make http request again
+        //We make the request if there's no loadedposts or id we do have pne but the IDs are different
+            if ( !this.state.loadedPost ||  (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+                    .then(response => {
+                        //console.log(response);
+                        this.setState({loadedPost: response.data})
+                    });
+            }
+    }
+}
     render () {
-        let post = <p>Please select a Post!</p>;
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        if(this.state.loadedPost) {             //initially its null cuz fetching data async hence above if code as waiting period
         post = (
             <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
+                <h1>{this.state.loadedPost.title}</h1>
+                <p>{this.state.loadedPost.body}</p>
                 <div className="Edit">
                     <button className="Delete">Delete</button>
                 </div>
             </div>
-
         );
+    }
         return post;
     }
 }
