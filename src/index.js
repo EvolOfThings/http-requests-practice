66a718f1746f7b54f4024db458e267b6db68be40
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-19 08:23:24 +0530

    adding axios interceptors for error handling globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+// global
+axios.interceptors.request.use(request => {
+    console.log(request);
+    //edit the request config and ALWAYS return
+    return request;
+}, error => {
+    console.log(error);  //error handling for sending request for no net connectivity
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    //edit the request config and ALWAYS return
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(
+  <App />,
+  rootEl
+)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    )
+  })
+}
+
 registerServiceWorker();
